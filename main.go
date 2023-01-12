3ba5adcfcd11edb131fd2c9f602b2a053b49f16f
StepanPokhodzhai <stelkerstepan@gmail.com>
2023-01-12 20:59:41 +0200

    Create main.go
    If you know you know

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go checkLink(l, c)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(err, link)
+		c <- link
+		return
+	}
+	fmt.Println(link)
+	c <- link
+
+}
